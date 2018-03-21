0c900f607ecb501642659c4116c6c56074050812
Kaviprakash156 <36467048+Kaviprakash156@users.noreply.github.com>
2018-03-21 05:02:52 -0700

    Create 3minnumarry.py

---
+++ 3minnumarry.py
@@ -0,0 +1,7 @@
+a=[]
+n=int(input("Enter the number of integers:\n"))
+for i in range(0,n):
+        m=int(input("Enter the number:\n"))
+        a.append(m)
+print (a)
+print ("Minimum value in an array is:\n",min(a))
