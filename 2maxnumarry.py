ed5f221470b47c0dc44b39387922382d06dad63c
Kaviprakash156 <36467048+Kaviprakash156@users.noreply.github.com>
2018-03-21 05:00:20 -0700

    Create 2maxnumarry.py

---
+++ 2maxnumarry.py
@@ -0,0 +1,7 @@
+a=[]
+n=int(input("Enter the number of integers:\n"))
+for i in range(0,n):
+        m=int(input("Enter the number:\n"))
+        a.append(m)
+print (a)
+print ("Maximum value in an array is:\n",max(a))
