1360b7347847278573f7caaf3bc224d8c6aaccc5
Kaviprakash156 <36467048+Kaviprakash156@users.noreply.github.com>
2018-05-24 14:53:17 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,13 +1,13 @@
-n=int(input("enter the number of elements:\n"))
+n=int(input())
 l=[]
 for i in range(0,n):
-  i=int(input("enter the element:\n"))
+  i=int(input())
   l.append(i)
   print (l)
 l.sort()
 print (l)
 k = len(l)
-if (k % 2 == 0):
+if k % 2 == 0:
     median = (l[(k)//2] + l[(k)//2-1]) / 2
 else:
     median = l[(k-1)//2]
